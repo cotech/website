b14658f7b860699d6a8171c0a5fba95c2c1fc7b6
oskar <oskar@outlandish.com>
2016-12-06 15:10:08 +0000

    Added fonts and icons to local directory structure

--- web/app/themes/coop-tech-oowp-theme/gulpfile.js
+++ web/app/themes/coop-tech-oowp-theme/gulpfile.js
@@ -9,7 +9,8 @@ var paths = {
     npm: '../../node_modules/',
     assets: elixir.config.assetsPath,
     public: elixir.config.publicPath,
-    foundation: '../../node_modules/foundation-sites/js/foundation/'
+    foundation: '../../node_modules/foundation-sites/js/foundation/',
+    foundationIcons: 'node_modules/foundation-icons/foundation-icons'
 };
 
 elixir(function(mix) {
@@ -36,10 +37,23 @@ elixir(function(mix) {
         ], paths.public + 'js/app.js')
 
         //images to public folder
-        .copy(paths.assets + 'images', paths.public + 'images/')
+        .copy(paths.assets + 'img', paths.public + 'img/')
 
         //move fonts to public folder
         .copy([
-            paths.npm + 'font-awesome/fonts'
-        ], paths.public + 'fonts/');
+            paths.assets + 'fonts',
+            'node_modules/font-awesome/fonts/'
+        ], paths.public + 'fonts/')
+
+        //move foundation-icons to public folder
+        .copy('node_modules/foundation-icons/svgs', paths.public + 'foundation-icons/svgs/')
+        .copy([
+            paths.foundationIcons + '.css',
+            paths.foundationIcons + '.eot',
+            paths.foundationIcons + '.scss',
+            paths.foundationIcons + '.svg',
+            paths.foundationIcons + '.ttf',
+            paths.foundationIcons + '.woff'
+        ], paths.public + 'foundation-icons/')
+    ;
 });
