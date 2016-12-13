4004750693b7abed724327ea62c50e1422ce921c
oskar <oskar@outlandish.com>
2016-12-13 17:24:34 +0000

    Added Google Maps API key

--- web/app/themes/coop-tech-oowp-theme/functions.php
+++ web/app/themes/coop-tech-oowp-theme/functions.php
@@ -23,9 +23,17 @@ if (class_exists('ooRoutemaster')) {
 /**
  * Put your WordPress functions here
  */
+function my_acf_google_map_api($api) {
+    if (GOOGLE_MAPS_API_KEY) {
+        $api['key'] = GOOGLE_MAPS_API_KEY;
+    }
+    return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_script('vendor', get_stylesheet_directory_uri() . '/public/js/vendor.js', [], false, true);
+    wp_enqueue_script('vendor', get_stylesheet_directory_uri() . '/public/js/vendor.js', [], false, false); //TODO in_footer = true?
     wp_enqueue_script('app', get_stylesheet_directory_uri() . '/public/js/app.js', ['vendor'], false, true);
     wp_enqueue_style('app', get_stylesheet_directory_uri() . '/public/css/app.css');
 });
