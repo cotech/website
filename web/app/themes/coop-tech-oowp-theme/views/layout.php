4ac3e7bdf466c2c5defc661e21e370a98ece677a
Chris Croome <chris@webarchitects.co.uk>
2017-01-04 13:44:45 +0000

    Added Webarchitects to the footer, hope that is OK!

--- web/app/themes/coop-tech-oowp-theme/views/layout.php
+++ web/app/themes/coop-tech-oowp-theme/views/layout.php
@@ -89,7 +89,9 @@
         <div class="row">
             <div class="small-12 columns">
                 <img src="<?php echo $coopLogo ?>" class="float-center" />
-                <p>Site by Glowbox Designs & Outlandish for CoTech &#xa9;2016</p>
+                <p>Site developed by <a href="http://www.glowboxdesign.co.uk/">Glowbox Designs</a> &amp; 
+                   <a href="http://outlandish.com/">Outlandish</a>, hosted by 
+                   <a href="https://www.webarchitects.coop/">Webarchitects</a> for CoTech &#xa9;2017</p>
             </div>
         </div>
     </div>
