c8b1cc75df68927d596e76f77a111b8f3b09e3de
Nick Sellen <git@nicksellen.co.uk>
2017-06-30 13:30:03 +0200

    Nicer output messages

--- docker/assets/download-images.sh
+++ docker/assets/download-images.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-BASEDIR="/app/web"
+URL="https://www.coops.tech/cotech-images.tgz"
+DEST="/app/web"
 
 # we just check for one path that we assume will be created when we unpack
-TEST_PATH="$BASEDIR/app/uploads/2017"
+TEST_PATH="$DEST/app/uploads/2017"
 
 if [ ! -e $TEST_PATH ] || [[ $* == *--force* ]]; then
-  echo "Downloading WordPress images"
-  curl -s https://www.coops.tech/cotech-images.tgz | tar -zxf - -C $BASEDIR
+  echo "Downloading WordPress images from $URL"
+  curl -s $URL | tar -zxf - -C $DEST
   echo "Images downloaded!"
 else
-  echo "Images already present, skipping download. Use --force to force update."
+  echo "Images already present, skipping download. Use --force to force download."
 fi
